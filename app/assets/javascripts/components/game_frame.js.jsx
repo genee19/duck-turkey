3473d32915411e4ef123376133b215d38037e322
Eugene Efimochkin <rimmer@ecwid.com>
2016-02-28 21:50:41 +0300

    Added the real fields to Grame.Frame component

--- app/assets/javascripts/components/game_frame.js.jsx
+++ app/assets/javascripts/components/game_frame.js.jsx
@@ -1,7 +1,26 @@
-var GameFrame = React.createClass({
+window.Game.Frame = React.createClass({
+  propTypes: {
+    frame: React.PropTypes.object,
+    is_final: React.PropTypes.bool
+  },
+  getInitialState: function(){
+      return {
+        rolls: [],
+        score: 0
+    };
+  },
+  inputs: function() {
+
+  },
   render: function() {
     return (
-    	<p>Frame</p>
-	)
+      <div className="game-frame">
+        <div className="game-frame__container game-frame__container--inputs">
+          <input name="rolls[]" type="number"/>
+          <input name="rolls[]" type="number"/>
+        </div>
+        <div className="game-frame__ouput--score"></div>
+      </div>
+    );
   }
 });
