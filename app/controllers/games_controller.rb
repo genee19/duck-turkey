d0c0cfcdd3f3a8c3c13c35e68eadb47a474c5601
Eugene Efimochkin <rimmer@ecwid.com>
2016-02-28 22:19:17 +0300

    Returning a JSON upon AJAX request

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,5 +3,10 @@ class GamesController < ApplicationController
 	end
 
 	def new
+		game = Game.new
+		game.rolls = params[:rolls].reject(&:'blank?').map(&:to_i) or []
+		result = game.scores
+
+		render json: result.marshal_dump.to_json
 	end
 end
