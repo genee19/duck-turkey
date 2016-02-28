2f6e5e29350c85d60cff91d961348fe474c2c872
Eugene Efimochkin <rimmer@ecwid.com>
2016-02-28 21:59:14 +0300

    Rendering frames ni funcitonal manner

--- app/assets/javascripts/components/game.js.jsx
+++ app/assets/javascripts/components/game.js.jsx
@@ -7,6 +7,13 @@ window.Game = React.createClass({
 	    	"over":false
     	};
 	},
+	frames: function() {
+		var result = [];
+		for (var frame in this.state.frames) {
+			result.push(<Game.Frame frame={frame} />);
+		}
+		return result;
+	},
 	newFrameWhenNecessary: function() {
 		if (!this.state.write_to_last_frame) {
 			return (
@@ -19,6 +26,7 @@ window.Game = React.createClass({
 		return (
 			<div className="game">
 				<form action={this.props.url} onChange={this.handleChange} className="game__form">
+					{this.frames()}
 					{this.newFrameWhenNecessary()}
 				</form>
 			</div>
